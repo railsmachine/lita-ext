b99c6964451b060aa395219031bbd7cfb06309b0
Bryan Traywick <bryan@railsmachine.com>
2014-03-24 02:48:53 -0400

    Fix bundler issue.
    Some things automatically loaded by lita-ext can require other gems.
    Specifically the initializers often require other gems to be loaded.
    However, when Lita calls Bundler.require lita-ext could be required
    before gems that are needed. Moving all of the lita-ext magic to an
    extended Lita.run method that performs all of the magic and then calls
    the standard Lita.run method to start Lita.

--- lib/lita/ext/core.rb
+++ lib/lita/ext/core.rb
@@ -1,5 +1,6 @@
 require 'lita'
 require 'active_support'
+require 'active_support/core_ext/module/aliasing'
 
 module Lita
   class << self
@@ -9,6 +10,54 @@ module Lita
 
     def root
       @root ||= ENV['LITA_ROOT'] ||= File.expand_path('.')
+    end
+
+    def run_with_ext(config_path = nil)
+      chdir_to_lita_root
+      add_lib_to_load_path
+      load_initializers
+      load_app_handlers
+      register_app_handlers
+      load_environment_config
+
+      run_without_ext(config_path)
+    end
+    alias_method_chain :run, :ext
+
+    private
+
+    def chdir_to_lita_root
+      Dir.chdir(Lita.root)
+    end
+
+    def add_lib_to_load_path
+      lib = File.expand_path('lib', Lita.root)
+      $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+    end
+
+    def load_initializers
+      initializers = "#{Lita.root}/config/initializers/**/*.rb"
+      Dir.glob(initializers).each { |initializer| require initializer }
+    end
+
+    def load_app_handlers
+      handlers = "#{Lita.root}/app/handlers/**/*.rb"
+      Dir.glob(handlers).each { |handler| require handler }
+    end
+
+    def register_app_handlers
+      Lita::Handler.handlers.each do |handler|
+        unless handler.disabled?
+          Lita.register_handler(handler)
+        end
+      end
+    end
+
+    def load_environment_config
+      environment = "#{Lita.root}/config/environments/#{Lita.env}"
+      if File.exists?("#{environment}.rb")
+        require environment
+      end
     end
   end
 end
