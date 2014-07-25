7c762e0eda63adce8ebf918aa5cbef3078ab5422
Bryan Traywick <bryan@railsmachine.com>
2014-07-24 21:58:08 -0400

    The methods of a Lita extension must be class methods.

--- lib/lita/ext/core.rb
+++ lib/lita/ext/core.rb
@@ -16,53 +16,55 @@ module Lita
 
   module Ext
     class Core
-      def self.call(payload)
-        chdir_to_lita_root
-        load_dotenv
-        add_lib_to_load_path
-        load_initializers
-        load_app_handlers
-        register_app_handlers
-        load_environment_config
-      end
+      class << self
+        def call(payload)
+          chdir_to_lita_root
+          load_dotenv
+          add_lib_to_load_path
+          load_initializers
+          load_app_handlers
+          register_app_handlers
+          load_environment_config
+        end
 
-      private
+        private
 
-      def chdir_to_lita_root
-        Dir.chdir(Lita.root)
-      end
+        def chdir_to_lita_root
+          Dir.chdir(Lita.root)
+        end
 
-      def load_dotenv
-        Dotenv.load ".env.#{Lita.env}", '.env'
-      end
+        def load_dotenv
+          Dotenv.load ".env.#{Lita.env}", '.env'
+        end
 
-      def add_lib_to_load_path
-        lib = File.expand_path('lib', Lita.root)
-        $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-      end
+        def add_lib_to_load_path
+          lib = File.expand_path('lib', Lita.root)
+          $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+        end
 
-      def load_initializers
-        initializers = "#{Lita.root}/config/initializers/**/*.rb"
-        Dir.glob(initializers).each { |initializer| require initializer }
-      end
+        def load_initializers
+          initializers = "#{Lita.root}/config/initializers/**/*.rb"
+          Dir.glob(initializers).each { |initializer| require initializer }
+        end
 
-      def load_app_handlers
-        handlers = "#{Lita.root}/app/handlers/**/*.rb"
-        Dir.glob(handlers).each { |handler| require handler }
-      end
+        def load_app_handlers
+          handlers = "#{Lita.root}/app/handlers/**/*.rb"
+          Dir.glob(handlers).each { |handler| require handler }
+        end
 
-      def register_app_handlers
-        Lita::Handler.handlers.each do |handler|
-          unless handler.disabled?
-            Lita.register_handler(handler)
+        def register_app_handlers
+          Lita::Handler.handlers.each do |handler|
+            unless handler.disabled?
+              Lita.register_handler(handler)
+            end
           end
         end
-      end
 
-      def load_environment_config
-        environment = "#{Lita.root}/config/environments/#{Lita.env}"
-        if File.exists?("#{environment}.rb")
-          require environment
+        def load_environment_config
+          environment = "#{Lita.root}/config/environments/#{Lita.env}"
+          if File.exists?("#{environment}.rb")
+            require environment
+          end
         end
       end
     end
